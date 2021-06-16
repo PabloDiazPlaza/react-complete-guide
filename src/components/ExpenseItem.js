746132177f480faff93a0ff488980d7ec7a3a2f5
PabloDiazPlaza <isaias.pablo.plaza@gmail.com>
2021-06-15 22:14:41 -0400

    commit Master

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Fecha</div>
+      <div>
+        <h2>Título</h2>
+        <div>Cantidad</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
